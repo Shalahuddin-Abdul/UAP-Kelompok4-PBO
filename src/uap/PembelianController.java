d214fa7f981bbbe13a571d990f491483c4a7be86
Shalahuddin-Abdul <94779696+Shalahuddin-Abdul@users.noreply.github.com>
2022-12-17 13:52:02 +0700

    Update PembelianController.java

--- src/uap/PembelianController.java
+++ src/uap/PembelianController.java
@@ -1,26 +1,47 @@
 package uap;
 
+import db.DBHelper;
+import static db.DBHelper.getConnection;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import jdbc.PenjualanModel;
+import uap.Classes.Makanan;
+import uap.Classes.Penjualan;
 
-public class PembelianController {
+public class PembelianController implements Initializable{
 
+    //buttons
     @FXML
     private Button addBtn;
-    
-    @FXML
-    private Button btnCheckout;
 
     @FXML
-    private Button pembBackBtn;
+    private Button btnCheckout;
     
+    @FXML
+    private Button pembBackBtn;
+
+    //textFields
     @FXML
     private TextField discField;
 
@@ -33,10 +54,24 @@ public class PembelianController {
     @FXML
     private TextField jmlField;
 
+    //combo box
+    @FXML
+    private ComboBox<String> dropdownMkn;
+    
+   
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       showMakanan();
+    }
+
+    
 
     @FXML
     void addToCart(ActionEvent event) {
+        PenjualanModel pjl = new PenjualanModel();
         
+//        Penjualan pjl1 = new Penjualan();
     }
     
     @FXML
@@ -55,4 +90,26 @@ public class PembelianController {
         stage.setScene(new Scene(root));
     }
 
+    private void showMakanan() {
+        ObservableList<String> mknList = FXCollections.observableArrayList();
+        Connection conn = getConnection();
+        String query = "SELECT * from mkn;";
+        Statement st;
+        ResultSet rs;
+        
+        try{
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+            while(rs.next()){
+                mknList.add(rs.getString("nama_produk"));
+            }
+            
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+        dropdownMkn.setItems(mknList);
+    }
+
+    
 }
