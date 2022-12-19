cbc84f399ec3beb8fa9e073fd5166d8ad448d699
Shalahuddin-Abdul <94779696+Shalahuddin-Abdul@users.noreply.github.com>
2022-12-19 12:49:04 +0700

    Update BarangModel.java

--- src/jdbc/BarangModel.java
+++ src/jdbc/BarangModel.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import uap.Classes.Barang;
 import uap.Classes.Makanan;
 
@@ -28,19 +29,21 @@ public class BarangModel {
     }
     //addMakanan
     public void addBarangSQL(Barang brg){
-        String insert = "INSERT INTO brg VALUES ('" + brg.getNama_produk() + "', " + brg.getHarga() + ", " + brg.getJumlah() + ", " + brg.getDiskon() + ", null, '" + brg.getBarcode() + "', '" + brg.getExpired() + "');";
+        String insert = "INSERT INTO brg VALUES ('" + brg.getNama_produk() + "', " + brg.getHarga() + ", " + brg.getJumlah() + ", " + brg.getDiskon() + ", null, '" + brg.getBarcode() + "', '" + brg.getExpired() + "', '" + brg.getKategori()+"');";
 //        System.out.println(insert);
         try {
             if(CONN.createStatement().executeUpdate(insert)>0){
-                System.out.println("Berhasil Memasukkan Data");
+                JOptionPane.showMessageDialog(null, "Berhasil Memasukkan Data");
                 status = true;
             }else{
-                System.out.println("Gagal Memasukkan Data");
+                JOptionPane.showMessageDialog(null, "Gagal Memasukkan Data");
+//                System.out.println("Gagal Memasukkan Data");
                 status = false;
             }
         } catch (SQLException ex) {
             Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Gagal Memasukkan Data");
+//            System.out.println("Gagal Memasukkan Data");
+            JOptionPane.showMessageDialog(null, "Gagal Memasukkan Data");
             status = false;
         }
     }
@@ -49,15 +52,19 @@ public class BarangModel {
         String delete = "DELETE FROM brg WHERE brg.nama_produk='" + brg.getNama_produk() + "';";
         try {
             if(CONN.createStatement().executeUpdate(delete)>0){
-                System.out.println("Berhasil Menghapus Data");
+                JOptionPane.showMessageDialog(null, "Berhasil Menghapus Data");
+//                System.out.println("Berhasil Menghapus Data");
                 status = true;
             }else{
-                System.out.println("Gagal Menghapus Data");
+//                System.out.println("Gagal Menghapus Data");
+                JOptionPane.showMessageDialog(null, "Gagal Menghapus Data");
                 status = false;
             }
         } catch (SQLException ex) {
             Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Gagal Menghapus Data");
+            JOptionPane.showMessageDialog(null, "Gagal Menghapus Data");
+
             status = false;
         }
     }
@@ -92,6 +99,40 @@ public class BarangModel {
             System.out.println("Berhasil Mengambil Data");
         }
         return brg;
+    }
+    
+    
+    public void updHargaMakananSQL(Barang brg, String namaField, String hargaField, String jmlField, String discField, String bcField, String expField){
+        int index = 0 ;
+        if(hargaField.isEmpty() && jmlField.isEmpty() && discField.isEmpty() && bcField.isEmpty() && expField.isEmpty() == false){
+            
+        }
+        String brgHarga = "brg.harga";
+        String brgNamaProduk = "brg.nama_produk";
+        String update = null;
+        Double hargaDbl = Double.parseDouble(hargaField);
+        switch(index){
+            //ubah harga sesuai nama barang
+            case 1:
+                update = "UPDATE brg SET " + brgHarga + " '"+ hargaDbl + "' WHERE brg.nama_produk='" + brg.getNama_produk() + "';";
+                break;
+            //ubah nama barang sesuai nama barang
+            case 2:
+                update = "UPDATE brg SET " + brgNamaProduk + " '"+ brg.getHarga() + "' WHERE brg.nama_produk='" + brg.getNama_produk() + "';";
+                break;
+            //ubah
+        }
+        
+        try {
+            if(CONN.createStatement().executeUpdate(update)>0){
+                System.out.println("Berhasil Update Data");
+            }else{
+                System.out.println("Gagal Update Data");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal Update Data");
+        }
     }
 }
 
